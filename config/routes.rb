9eeaf7b1245c6c578a49c0857b896dc1013c115f
Monica <monica.arnon@gmail.com>
2016-02-08 23:58:15 +0000

    Backtracked routes in favor of new branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # get 'welcome/index'
 
   resources :questions
+  
   devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
