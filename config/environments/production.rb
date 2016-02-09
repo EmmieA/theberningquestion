ce08085ec69c06bd0675642a6507c7f5fd5ad130
Monica <monica.arnon@gmail.com>
2016-02-09 00:28:24 +0000

    Finished email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { 
-  	:host => 'yourherokuappname.herokuapp.com', :protocol => 'https'
+  	:host => 'theberningquestion.herokuapp.com', :protocol => 'https'
   }
   # Code is not reloaded between requests.
   config.cache_classes = true
