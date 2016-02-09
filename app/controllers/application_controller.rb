c235b4b1cdcd162a8ee9c6edbd2c5f14ca8d12c7
Monica <monica.arnon@gmail.com>
2016-02-09 01:42:44 +0000

    User name storage, additional navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
   
   before_action :authenticate_user!
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
 end
