46752788db4dcd93edd0ee28d2f9a5d62390cb9e
Monica <monica.arnon@gmail.com>
2016-02-11 00:09:12 +0000

    Added gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,5 +18,12 @@ module ApplicationHelper
     
     _parsed_tweet.html_safe
   end
+
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+  end
   
 end
